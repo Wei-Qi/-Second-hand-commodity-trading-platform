b59e8166a06e9ce6a81523e929f7be8ee7b56ff4
Wei-Qi <60121955+Wei-Qi@users.noreply.github.com>
2022-05-19 10:34:39 +0800

    暂存

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from flask_migrate import Migrate
 from models import *
 from Function import function
 
-
 login_manager = LoginManager()
 
-login_manager.login_view = '/user/login'    #未登录将自动跳转到该路径
+login_manager.login_view = '/user/login'  # 未登录将自动跳转到该路径
 login_manager.login_message = '请先登陆'
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -32,6 +32,7 @@ app.register_blueprint(user_bp)
 @app.route('/home')
 def home():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
