769945af4e73c0241eca1aa4f182a239a19e3958
Wei-Qi <60121955+Wei-Qi@users.noreply.github.com>
2022-05-19 18:23:17 +0800

    修改了用户名不能修改的bug

--- User/user.py
+++ User/user.py
@@ -144,10 +144,10 @@ class user:
         :param name:姓名
         :param sex:性别
         :param phone:手机号码
-        :return: '改昵称已经被注册' or '用户id不存在' or True
+        :return: '该昵称已经被注册' or '用户id不存在' or True
         """
         user = UserModel.query.filter_by(UserName=name).first()
-        if user is not None:
+        if user is not None and user.UserId != id:
             return '该昵称已经被注册'
         user = UserModel.query.filter_by(UserId=id).first()
         if user is None:
