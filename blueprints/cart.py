7e5937d0b0c20dc76ed4b9fe314eb2401a19710c
DragonYang <56200279+DragonYang-code@users.noreply.github.com>
2022-05-28 19:41:09 +0800

    更新了购物车的蓝图

--- blueprints/cart.py
+++ blueprints/cart.py
@@ -12,6 +12,38 @@ from Cart.Cart import Cart
 bp=Blueprint('cart',__name__,url_prefix="/cart")
 
 @bp.route('/')
+@login_required
 def cart():
-    return render_template('cart.html')
 
+    cart_items=Cart.get_cart_by_usrid(current_user.get_id())
+    return render_template('cart.html',cart_items=cart_items)
+
+@bp.route('/delete_item/<int:itemid>')
+@login_required
+def deleteItem(itemid):
+    res=Cart.del_cart_by_id(itemid)
+    if res is not True:
+        flash(res)
+    return redirect('/cart')
+
+@bp.route('/add_item/<int:goodsid>',methods=['POST'])
+@login_required
+def addItem(goodsid):
+    cnt=int(request.form.get('cnt',1))
+    res=Cart.add_cart(current_user.get_id(),goodsid,cnt)
+    if res is True:
+        return jsonify({'code':200,'message':'添加成功,请到购物车中查看'})
+    else:
+        return jsonify({'code':400,'message':res})
+
+@bp.route('/set_cnt/<int:itemid>',methods=['POST'])
+@login_required
+def setCnt(itemid):
+    cnt=request.form.get('cnt')
+    res=Cart.change_goods_num(itemid,cnt)
+    if res is True:
+        return jsonify({'code':200,'message':'修改成功'})
+    else:
+        return jsonify({'code':400,'message':res})
+
+/cart/set_cnt/
